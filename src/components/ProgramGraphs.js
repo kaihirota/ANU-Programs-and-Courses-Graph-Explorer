c9215f7dbcd7470ad8863d51296152874cb84710
Kai Hirota <kaiyhirota@gmail.com>
2021-12-28 03:52:16 +1100

    fix height of canvas element and program graph visualisation

--- src/components/ProgramGraphs.js
+++ src/components/ProgramGraphs.js
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import { useQuery, gql } from '@apollo/client'
 import {
-  Box,
+  CircularProgress,
   FormControl,
   InputLabel,
   Select,
   MenuItem,
 } from '@material-ui/core'
 import Neovis from 'neovis.js/dist/neovis.js'
-
 import Title from './Title'
-// import { NeoGraph, ResponsiveNeoGraph } from './NeoGraph'
+import './graph.css'
 
+const CompletionEvent = 'completed'
 const QUERY_GET_PROGRAMS = gql`
   {
     programs(options: { limit: 50, skip: 80 }) {
@@ -20,63 +20,67 @@ const QUERY_GET_PROGRAMS = gql`
     }
   }
 `
-
 const neo4jUri = process.env.REACT_APP_NEO4J_URI || 'localhost:7687'
 const neo4jUser = process.env.REACT_APP_NEO4J_USER || 'neo4j'
 const neo4jPassword = process.env.REACT_APP_NEO4J_PASSWORD || 'neo4j'
 
 export default function ProgramGraphs() {
+  const [loading_curr, setLoading] = useState(false)
   const { loading, error, data } = useQuery(QUERY_GET_PROGRAMS)
   if (error) return <p>Error</p>
   if (loading) return <p>Loading</p>
 
-  const config = {
-    container_id: 'program-vis',
-    server_url: neo4jUri,
-    server_user: neo4jUser,
-    server_password: neo4jPassword,
-    labels: {
-      Course: {
-        caption: 'name',
-        title_properties: ['id', 'name', 'units'],
-      },
-      Program: {
-        caption: 'name',
-        title_properties: ['id', 'name', 'units'],
-      },
-      Specialisation: {
-        caption: 'name',
-        title_properties: ['id', 'name'],
-      },
-      Requirement: {
-        caption: 'units',
-        title_properties: ['units', 'description'],
-      },
-    },
-    relationships: {
-      REQUIREMENT: {
-        caption: true,
-      },
-    },
-    hierarchical: true,
-    initial_cypher: `MATCH p=(:Program {name: 'Master of Machine Learning and Computer Vision'})-[r:REQUIREMENT*1..]->() RETURN p`,
-  }
-
-  const vis = new Neovis(config)
-  vis.render()
-
   const handleChange = (e) => {
-    console.log(e.target.value)
-    vis.renderWithCypher(
-      `MATCH p=(:Program {id: '${e.target.value}'})-[r:REQUIREMENT*1..]->() RETURN p`
-    )
+    document.getElementById('graph-vis').innerHTML = ''
+    setLoading(true)
+    const config = {
+      container_id: 'graph-vis',
+      server_url: neo4jUri,
+      server_user: neo4jUser,
+      server_password: neo4jPassword,
+      labels: {
+        Course: {
+          caption: 'id',
+          title_properties: ['id', 'name', 'units'],
+        },
+        Specialisation: {
+          caption: 'name',
+          title_properties: ['id', 'name'],
+        },
+        Program: {
+          caption: 'name',
+          title_properties: ['id', 'name', 'units'],
+        },
+        Requirement: {
+          caption: 'units',
+          title_properties: ['units', 'description'],
+        },
+      },
+      relationships: {
+        REQUIREMENT: {
+          caption: false,
+          thickness: 'none',
+        },
+      },
+      arrows: true,
+      hierarchical: false,
+      //   hierarchical: true,
+      //   hierarchical_sort_method: 'directed',
+      initial_cypher: `MATCH p=(:Program {id: '${e.target.value}'})-[r:REQUIREMENT*1..]->() RETURN p`,
+    }
+
+    const vis = new Neovis(config)
+    vis.registerOnEvent(CompletionEvent, () => {
+      setLoading(false)
+    })
+    vis.render()
   }
 
   return (
     <React.Fragment>
       <Title>Program Graph</Title>
       <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Program</InputLabel>
+        <InputLabel id="demo-simple-select-label">Select degree</InputLabel>
         <Select label="Program Name" onChange={handleChange}>
           {data.programs.map((n) => {
             return (
@@ -87,7 +91,12 @@ export default function ProgramGraphs() {
           })}
         </Select>
       </FormControl>
-      <div id="program-vis" width="800" height="600"></div>
+      <div className="graph-and-loading-icon-wrapper">
+        <div className="loading-icon-wrapper">
+          {loading && <CircularProgress size={100} />}
+        </div>
+        <div id="graph-vis"></div>
+      </div>
     </React.Fragment>
   )
 }
