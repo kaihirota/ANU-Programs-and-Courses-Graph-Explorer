fd791c0399896aeed75f8eaa9eb94bb74ddf8f6f
Kai Hirota <kaiyhirota@gmail.com>
2022-01-01 04:32:16 +1100

    update

--- src/index.js
+++ src/index.js
@@ -17,14 +17,4 @@ const Main = () => (
 )
 
 ReactDOM.render(<Main />, document.getElementById('root'))
-registerServiceWorker().then(
-  function (registration) {
-    console.log(
-      'ServiceWorker registration successful with scope: ',
-      registration.scope
-    )
-  },
-  function (err) {
-    console.log('ServiceWorker registration failed: ', err)
-  }
-)
+registerServiceWorker()
