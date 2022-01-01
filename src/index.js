daa65c82fb8cf2ec2328833fc964eeb3ce6c89e2
Kai Hirota <kaiyhirota@gmail.com>
2022-01-01 22:29:27 +1100

    - disable serviceworker

--- src/index.js
+++ src/index.js
@@ -17,8 +17,8 @@ const Main = () => (
 )
 
 ReactDOM.render(<Main />, document.getElementById('root'))
-registerServiceWorker()
+// registerServiceWorker()
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-unregister()
+// unregister()
