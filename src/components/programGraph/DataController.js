a9dc697b4c9a8bdf4424b20c59ab47c57f3259d3
Kai Hirota <kaiyhirota@gmail.com>
2022-01-09 22:31:37 +1100

    - customize rendering for node labels and hover labels

--- src/components/programGraph/DataController.js
+++ src/components/programGraph/DataController.js
@@ -2,8 +2,8 @@ import React, { useContext, useEffect } from 'react'
 import { useSigma } from 'react-sigma-v2'
 import PropTypes from 'prop-types'
 
-import { random } from 'graphology-layout'
-import forceAtlas2 from 'graphology-layout-forceatlas2'
+import { circular } from 'graphology-layout'
+import noverlap from 'graphology-layout-noverlap'
 import { SelectedCoursesContext, SelectedProgramContext } from '../../contexts'
 
 const COLORMAP = {
@@ -51,13 +51,17 @@ const DataController = (props) => {
     })
 
     // set layout
-    random.assign(graph)
-    forceAtlas2.assign(graph, {
-      iterations: 50,
-      settings: {
-        gravity: 10,
-      },
-    })
+    // random.assign(graph)
+    circular.assign(graph)
+    // forceLayout.assign(graph, {
+    //   maxIterations: 100,
+    //   settings: {
+    //     attraction: 0.5, // 0.0005: importance of the attraction force, that attracts each pair of connected nodes like elastics.
+    //     repulsion: 1, // 0.1: importance of the repulsion force, that attracts each pair of nodes like magnets.
+    //     gravity: 0.001, // 0.0001: importance of the gravity force, that attracts all nodes to the center.
+    //   },
+    // })
+    noverlap.assign(graph)
 
     return () => graph.clear()
   }, [dataset])
@@ -82,16 +86,35 @@ const DataController = (props) => {
             graph.forEachOutNeighbor(root, (neighbor) => {
               units += DFS(neighbor)
             })
-
             if (attributes.tag === 'Requirement') {
-              graph.setNodeAttribute(
-                root,
-                'label',
-                `${units}/${attributes.units}`
-              )
+              if (attributes.units) {
+                graph.setNodeAttribute(
+                  root,
+                  'label',
+                  `${units}/${attributes.units}`
+                )
 
-              if (units >= attributes.units) {
-                graph.setNodeAttribute(root, 'color', COLORMAP.Complete)
+                if (units >= attributes.units) {
+                  graph.setNodeAttribute(root, 'color', COLORMAP.Complete)
+                }
+              } else {
+                graph.setNodeAttribute(root, 'label', attributes.description)
+                graph.forEachOutNeighbor(
+                  root,
+                  (neighbor, neighborAttributes) => {
+                    if (
+                      neighborAttributes.color &&
+                      neighborAttributes.color === COLORMAP.Complete
+                    ) {
+                      graph.setNodeAttribute(root, 'color', COLORMAP.Complete)
+                      graph.setNodeAttribute(
+                        root,
+                        'units',
+                        neighborAttributes.units
+                      )
+                    }
+                  }
+                )
               }
             }
           }
