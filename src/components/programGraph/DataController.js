c275caaa6ad639272950db19d8c8181f78c55f2c
Kai Hirota <kaiyhirota@gmail.com>
2022-01-09 19:59:49 +1100

    color nodes based on completion status of children nodes

--- src/components/programGraph/DataController.js
+++ src/components/programGraph/DataController.js
@@ -1,24 +1,32 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { useSigma } from 'react-sigma-v2'
 import PropTypes from 'prop-types'
-import FA2Layout from 'graphology-layout-forceatlas2/worker'
 
 import { random } from 'graphology-layout'
 import forceAtlas2 from 'graphology-layout-forceatlas2'
+import { SelectedCoursesContext, SelectedProgramContext } from '../../contexts'
+
+const COLORMAP = {
+  Program: '#5454FF',
+  Specialisation: '#AA47FF',
+  Requirement: '#FFBC47',
+  Course: '#FF5454',
+  Complete: '#54FF54',
+}
 
 const DataController = (props) => {
+  const { programId, setProgramId } = useContext(SelectedProgramContext)
+  const { selectedCourses, setSelectedCourses } = useContext(
+    SelectedCoursesContext
+  )
   const { dataset, children } = props
   const sigma = useSigma()
   const graph = sigma.getGraph()
-  // const [clusters, setClusters] = useState({})
 
-  /**
-   * Feed graphology with the new dataset:
-   */
+  // Feed graphology with the new dataset:
   useEffect(() => {
-    if (!graph || !dataset) return
+    if (!dataset || !graph) return
 
-    // build graph
     dataset.nodes
       .filter((node) => node.id)
       .forEach((node) => {
@@ -50,31 +58,53 @@ const DataController = (props) => {
         gravity: 10,
       },
     })
-    // const layout = new FA2Layout(graph, {
-    //   settings: { gravity: 10 },
-    //   weighted: false,
-    //   iterations: 10,
-    // })
-    // layout.start()
-
-    // apply colors
-    let labelToColorMap = {}
-    dataset.tags.forEach((t) => {
-      labelToColorMap[t.key] = t.color
-    })
-    graph.forEachNode((node, { tag }) =>
-      graph.setNodeAttribute(node, 'color', labelToColorMap[tag])
-    )
 
     return () => graph.clear()
   }, [dataset])
 
   useEffect(() => {
-    console.log(dataset)
-  }, [graph, dataset])
+    if (graph.nodes().length > 0) {
+      // apply color
+      graph.forEachNode((node, attributes) => {
+        graph.setNodeAttribute(node, 'color', COLORMAP[attributes.tag])
+      })
+
+      const DFS = (root) => {
+        // if req / specialisation, filter for outgoing neighbors that are in completed and sum units
+
+        let units = 0
+        if (root !== '') {
+          const attributes = graph.getNodeAttributes(root)
+          if (attributes.tag === 'Course' && selectedCourses.includes(root)) {
+            graph.setNodeAttribute(root, 'color', COLORMAP.Complete)
+            units = attributes.units
+          } else {
+            graph.forEachOutNeighbor(root, (neighbor) => {
+              units += DFS(neighbor)
+            })
+
+            if (attributes.tag === 'Requirement') {
+              graph.setNodeAttribute(
+                root,
+                'label',
+                `${units}/${attributes.units}`
+              )
+
+              if (units >= attributes.units) {
+                graph.setNodeAttribute(root, 'color', COLORMAP.Complete)
+              }
+            }
+          }
+        }
+        return units
+      }
+      DFS(programId)
+    }
+  }, [dataset, selectedCourses])
 
   return <>{children}</>
 }
+
 DataController.propTypes = {
   dataset: PropTypes.shape({
     nodes: PropTypes.array.isRequired,
