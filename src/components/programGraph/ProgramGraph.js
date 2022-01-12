1e9da93facecf38e95dcd47edc1d617ed1017bde
Kai Hirota <kaiyhirota@gmail.com>
2022-01-12 23:26:48 +1100

    switch from breadthfirst to klay layout

--- src/components/programGraph/ProgramGraph.js
+++ src/components/programGraph/ProgramGraph.js
@@ -1,34 +1,17 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../graph.css'
-import DataController from './DataController'
 import {
   extractDataset,
   NEO4J_PASSWORD,
   NEO4J_URI,
   NEO4J_USER,
 } from '../../utils'
-import GraphSettingsController from '../sigmaGraph/views/GraphSettingsController'
-import EventsController from './EventsController'
-import { drawHoverForProgram } from '../sigmaGraph/canvas-utils'
 import { useSelector } from 'react-redux'
-import PropTypes from 'prop-types'
-import CytoscapeComponent from 'react-cytoscapejs/src/component'
 import { CircularProgress } from '@material-ui/core'
 import cytoscape from 'cytoscape'
-import dagre from 'cytoscape-dagre'
-import elk from 'cytoscape-elk'
-import cola from 'cytoscape-cola'
-import avsdf from 'cytoscape-avsdf'
-import cise from 'cytoscape-cise'
-import coseBilkent from 'cytoscape-cose-bilkent'
-import fcose from 'cytoscape-fcose'
-cytoscape.use(dagre)
-// cytoscape.use(elk)
-// cytoscape.use(cola)
-// cytoscape.use(avsdf)
-// cytoscape.use(cise)
-// cytoscape.use(coseBilkent)
-// cytoscape.use(fcose)
+import klay from 'cytoscape-klay'
+
+cytoscape.use(klay)
 
 const neo4j = require('neo4j-driver')
 const driver = neo4j.driver(
@@ -78,30 +61,69 @@ export default function ProgramGraph(props) {
     return extracted
   }
   const [layout, setLayout] = useState({
-    name: 'breadthfirst',
-
-    fit: true, // whether to fit the viewport to the graph
-    directed: true, // whether the tree is directed downwards (or edges can point in any direction if false)
-    padding: 30, // padding on fit
-    circle: false, // put depths in concentric circles if true, put depths top down if false
-    grid: true, // whether to create an even grid into which the DAG is placed (circle:false only)
-    spacingFactor: 1.75, // positive spacing factor, larger => more space between nodes (N.B. n/a if causes overlap)
-    boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
-    avoidOverlap: true, // prevents node overlap, may overflow boundingBox if not enough space
-    nodeDimensionsIncludeLabels: false, // Excludes the label when calculating node bounding boxes for the layout algorithm
-    roots: undefined, // the roots of the trees
-    maximal: true, // whether to shift nodes down their natural BFS depths in order to avoid upwards edges (DAGS only)
-    animate: false, // whether to transition the node positions
-    animationDuration: 500, // duration of animation in ms if enabled
-    animationEasing: undefined, // easing of animation if enabled,
+    name: 'klay',
+    nodeDimensionsIncludeLabels: false, // Boolean which changes whether label dimensions are included when calculating node dimensions
+    fit: true, // Whether to fit
+    padding: 20, // Padding on fit
+    animate: false, // Whether to transition the node positions
     animateFilter: function (node, i) {
       return true
-    }, // a function that determines whether the node should be animated.  All nodes animated by default on animate enabled.  Non-animated nodes are positioned immediately when the layout starts
-    ready: undefined, // callback on layoutready
-    stop: undefined, // callback on layoutstop
-    transform: function (node, position) {
-      return position
+    }, // Whether to animate specific nodes when animation is on; non-animated nodes immediately go to their final positions
+    animationDuration: 500, // Duration of animation in ms if enabled
+    animationEasing: undefined, // Easing of animation if enabled
+    transform: function (node, pos) {
+      return pos
+    }, // A function that applies a transform to the final node position
+    ready: undefined, // Callback on layoutready
+    stop: undefined, // Callback on layoutstop
+    klay: {
+      // direction: 'horizontal', // Overall direction of edges: horizontal (right / left) or vertical (down / up)
+      direction: 'DOWN', // Overall direction of edges: horizontal (right / left) or vertical (down / up)
+      edgeSpacingFactor: 0.5, // Factor by which the object spacing is multiplied to arrive at the minimal spacing between edges.
+      // Following descriptions taken from http://layout.rtsys.informatik.uni-kiel.de:9444/Providedlayout.html?algorithm=de.cau.cs.kieler.klay.layered
+      addUnnecessaryBendpoints: false, // Adds bend points even if an edge does not change direction.
+      aspectRatio: 1.6, // The aimed aspect ratio of the drawing, that is the quotient of width by height
+      borderSpacing: 20, // Minimal amount of space to be left to the border
+      compactComponents: false, // Tries to further compact components (disconnected sub-graphs).
+      crossingMinimization: 'LAYER_SWEEP', // Strategy for crossing minimization.
+      /* LAYER_SWEEP The layer sweep algorithm iterates multiple times over the layers, trying to find node orderings that minimize the number of crossings. The algorithm uses randomization to increase the odds of finding a good result. To improve its results, consider increasing the Thoroughness option, which influences the number of iterations done. The Randomization seed also influences results.
+      INTERACTIVE Orders the nodes of each layer by comparing their positions before the layout algorithm was started. The idea is that the relative order of nodes as it was before layout was applied is not changed. This of course requires valid positions for all nodes to have been set on the input graph before calling the layout algorithm. The interactive layer sweep algorithm uses the Interactive Reference Point option to determine which reference point of nodes are used to compare positions. */
+      cycleBreaking: 'GREEDY', // Strategy for cycle breaking. Cycle breaking looks for cycles in the graph and determines which edges to reverse to break the cycles. Reversed edges will end up pointing to the opposite direction of regular edges (that is, reversed edges will point left if edges usually point right).
+      /* GREEDY This algorithm reverses edges greedily. The algorithm tries to avoid edges that have the Priority property set.
+      INTERACTIVE The interactive algorithm tries to reverse edges that already pointed leftwards in the input graph. This requires node and port coordinates to have been set to sensible values.*/
+      /* UNDEFINED, RIGHT, LEFT, DOWN, UP */
+      edgeRouting: 'POLYLINE', // Defines how edges are routed (POLYLINE, ORTHOGONAL, SPLINES)
+      feedbackEdges: false, // Whether feedback edges should be highlighted by routing around the nodes.
+      fixedAlignment: 'BALANCED', // Tells the BK node placer to use a certain alignment instead of taking the optimal result.  This option should usually be left alone.
+      /* NONE Chooses the smallest layout from the four possible candidates.
+      LEFTUP Chooses the left-up candidate from the four possible candidates.
+      RIGHTUP Chooses the right-up candidate from the four possible candidates.
+      LEFTDOWN Chooses the left-down candidate from the four possible candidates.
+      RIGHTDOWN Chooses the right-down candidate from the four possible candidates.
+      BALANCED Creates a balanced layout from the four possible candidates. */
+      inLayerSpacingFactor: 1.0, // Factor by which the usual spacing is multiplied to determine the in-layer spacing between objects.
+      layoutHierarchy: false, // Whether the selected layouter should consider the full hierarchy
+      linearSegmentsDeflectionDampening: 0.3, // Dampens the movement of nodes to keep the diagram from getting too large.
+      mergeEdges: true, // Edges that have no ports are merged so they touch the connected nodes at the same points.
+      mergeHierarchyCrossingEdges: true, // If hierarchical layout is active, hierarchy-crossing edges use as few hierarchical ports as possible.
+      nodeLayering: 'NETWORK_SIMPLEX', // Strategy for node layering.
+      /* NETWORK_SIMPLEX This algorithm tries to minimize the length of edges. This is the most computationally intensive algorithm. The number of iterations after which it aborts if it hasn't found a result yet can be set with the Maximal Iterations option.
+      LONGEST_PATH A very simple algorithm that distributes nodes along their longest path to a sink node.
+      INTERACTIVE Distributes the nodes into layers by comparing their positions before the layout algorithm was started. The idea is that the relative horizontal order of nodes as it was before layout was applied is not changed. This of course requires valid positions for all nodes to have been set on the input graph before calling the layout algorithm. The interactive node layering algorithm uses the Interactive Reference Point option to determine which reference point of nodes are used to compare positions. */
+      nodePlacement: 'BRANDES_KOEPF', // Strategy for Node Placement
+      /* BRANDES_KOEPF Minimizes the number of edge bends at the expense of diagram size: diagrams drawn with this algorithm are usually higher than diagrams drawn with other algorithms.
+      LINEAR_SEGMENTS Computes a balanced placement.
+      INTERACTIVE Tries to keep the preset y coordinates of nodes from the original layout. For dummy nodes, a guess is made to infer their coordinates. Requires the other interactive phase implementations to have run as well.
+      SIMPLE Minimizes the area at the expense of... well, pretty much everything else. */
+      randomizationSeed: 1, // Seed used for pseudo-random number generators to control the layout algorithm; 0 means a new seed is generated
+      routeSelfLoopInside: false, // Whether a self-loop is routed around or inside its node.
+      separateConnectedComponents: true, // Whether each connected component should be processed separately
+      spacing: 50, // Overall setting for the minimal amount of space to be left between objects
+      thoroughness: 7, // How much effort should be spent to produce a nice layout..
     },
+    priority: function (edge) {
+      return null
+    }, // Edges with a non-nil value are skipped when greedy edge cycle breaking is enabled
   })
 
   useEffect(() => {
@@ -189,7 +211,7 @@ export default function ProgramGraph(props) {
         style: {
           width: 1,
           // 'curve-style': 'bezier',
-          'line-color': '#4C4C4C',
+          'line-color': '#B3B3B3',
           'target-arrow-color': '#3A52E2',
           'target-arrow-shape': 'triangle',
           'overlay-opacity': 0,
